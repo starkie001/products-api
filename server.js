22946ab08256ba5930b630a054b3b9ee2ff73e5c
Davinder Singh <dasingh9@gmail.com>
2025-09-12 22:32:37 +1200

    Disable Swagger file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import ProductRouter from './routes/product-route.js';
 import { errorHandler } from './middleware/error-handler.js';
 import { checkApiKey } from './middleware/auth-handler.js';
 import dotenv from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
+//import swaggerUi from 'swagger-ui-express';
 //import swaggerDocument from './swagger.json' assert {type: 'json'};
 
 dotenv.config(); // Load env vars from .env file
@@ -13,11 +13,11 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+/*app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/api-docs/swagger.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
-});
+});*/
 
 
 
